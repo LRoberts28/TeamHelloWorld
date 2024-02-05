1b28d2ab1207d213dfe0b1ea98b89b583c829b97
LegoMan9231 <157411269+LegoMan9231@users.noreply.github.com>
2024-02-05 14:20:16 -0600

    push 1

---
+++ Game020524/GameHelper.java
@@ -0,0 +1,11 @@
+package Game020524;
+
+import java.util.Scanner;
+
+public class GameHelper {
+    public int getUserInput(String prompt) {
+        System.out.print(prompt + ": ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+}
