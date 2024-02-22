40a5262b86a994d10a92080b02a1d56dc8385abd
Slinky <lroberts28@murraystate.edu>
2024-02-22 16:12:22 -0600

    created person class based on rubric to the best of my ability

--- HrApp/Person.java
+++ HrApp/Person.java
@@ -1,5 +1,35 @@
 package HrApp;
 
 public class Person {
+    //attributes
+    private String name;
+    private int age;
 
+    //constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    //methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
 }
