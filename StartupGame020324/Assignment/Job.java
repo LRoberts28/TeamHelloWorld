03d0583e147a1c0b3b7fd4e6307565d18b39afa5
tpalmer8 <tpalmer8@murraystate.edu>
2024-02-23 13:07:08 -0600

    Created job

---
+++ StartupGame020324/Assignment/Job.java
@@ -0,0 +1,45 @@
+package StartupGame020324.Assignment;
+
+public class Job {
+    //attributes
+        //Stores the past or current job title
+        String title;
+    
+        //Stores the number of months in the job
+        int experience;
+    
+    //methods
+        //Sets the job title
+        public void setTitle(String jobTitle)
+        {
+            title = jobTitle;
+        }
+    
+        //Sets the number of months on the job.
+        public void setExperience(int exp)
+        {
+            experience = exp;
+        }
+    
+        //Retrieves the job title
+        public String getTitle()
+        {
+            return title;
+        }
+    
+        //Retrieves the number of months on the job
+        public int getExperience()
+        {
+            return experience;
+        }
+    
+        //Displays the jobs and lengths of time in months on the job
+        public void displayDetails()
+        {
+            System.out.println();
+            System.out.println("--- Jobs ---");
+            System.out.println("Title: " + title);
+            System.out.println("Experience " + experience + " months");
+            System.out.println();
+        }
+    }
