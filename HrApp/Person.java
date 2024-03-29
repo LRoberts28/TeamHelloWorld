4d1146323ee470d1da08b6b8a6775bad403e0673
Slinky <lroberts28@murraystate.edu>
2024-03-29 14:37:48 -0500

    Added more to Person Class. Not finished. Task list is at bottom.

--- HrApp/Person.java
+++ HrApp/Person.java
@@ -1,23 +1,59 @@
 package HrApp;
 
-public class Person {
-    //attributes
+public class Person { // class diagram says User.java but i didn't want to break code.
     private String name;
     private int age;
+    private String email;
+    private String password;
 
-    //constructor
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
+    // private SecurityClearance clearance;
+    // States = currentState=SecurityClearance.LOW
+
+    enum ClearanceLevel {
+        USER,
+        EMPLOYEE,
+        MANAGER;
     }
 
-    //methods
+    ClearanceLevel secLevel = ClearanceLevel.EMPLOYEE;
+    // System.out.println(c1);
+
+    public enum SecurityClearance {
+        LOW,
+        MEDIUM,
+        HIGH;
+    }
+
+    // constructor
+    public Person(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    // methods
+    public String setEmail(String email) {
+        return email;
+    }
+
+    public String setPassword(String password) {
+        return this.password = password;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public String getName() {
@@ -31,4 +67,35 @@ public class Person {
     public void displayDetails() {
         System.out.println("--- Person Details ---\r\n" + "Name: " + name + "\r\n" + "Age: " + age);
     }
+
+    // public int getSecurity() {
+    // int secLevel;
+    // }
 }
+
+/*
+ * User
+ * +email: String------------------------------------ // done
+ * +password: String------------------------------------ // done
+ * +name: String------------------------------------ // done
+ * +securityClearance: enum // working on
+ * +description: String //written to embedded file using BufferedWriter
+ * +tags: Enum
+ * +accountRecovery()
+ * +addTags()
+ * +getEmail()------------------------------------ // done
+ * +getPassword()------------------------------------ // done
+ * +getName()------------------------------------ // done
+ * +getSecurity() ---------- in progress-------------
+ * +getTags()
+ * +logIn()
+ * +save()
+ * +setPassword()------------------------------------ // done
+ * +setEmail()------------------------------------ // done
+ * +setName()------------------------------------ // done
+ * +setSecurity()
+ * +switchUser()
+ * +readDesc()
+ * +removeUser()
+ * +writeDesc()
+ */
