fb55238fefc2b9d72a72830552a7c59feef1a35d
Slinky <lroberts28@murraystate.edu>
2024-04-03 16:30:07 -0500

    Added enum class and functioning SecurityClearance for all classes of the HrApp package. Fixed errors and problems with Person and Employee class. Added securityClearance to HrMain.java to test and fix error.

--- HrApp/Person.java
+++ HrApp/Person.java
@@ -5,61 +5,62 @@ public class Person { // class diagram says User.java but i didn't want to break
     private int age;
     private String email;
     private String password;
+    private SecurityClearance clearance;
 
-    // private SecurityClearance clearance;
-    // States = currentState=SecurityClearance.LOW
-
-    enum ClearanceLevel {
-        USER,
-        EMPLOYEE,
-        MANAGER;
-    }
-
-    ClearanceLevel secLevel = ClearanceLevel.EMPLOYEE;
-    // System.out.println(c1);
-
-    public enum SecurityClearance {
-        LOW,
-        MEDIUM,
-        HIGH;
-    }
-
-    // constructor
-    public Person(String name, int age, String email) {
+    public Person(String name, int age, String email, String password, SecurityClearance clearance) {
         this.name = name;
         this.age = age;
         this.email = email;
+        this.password = password;
+        this.clearance = clearance;
     }
 
-    // methods
+    // method to set persons email
     public String setEmail(String email) {
         return email;
     }
 
-    public String setPassword(String password) {
-        return this.password = password;
+    // method to set persons password
+    public void setPassword(String password) {
+        this.password = password;
     }
 
+    // method to set persons name
     public void setName(String name) {
         this.name = name;
     }
 
+    // method to set persons age
     public void setAge(int age) {
         this.age = age;
     }
 
+    // method for setting the persons clearance level
+    public void setClearanceLevel(SecurityClearance clearance) {
+        this.clearance = clearance;
+    }
+
+    // method for getting the persons clearance level
+    public SecurityClearance getClearance() {
+        return clearance;
+    }
+
+    // method to get persons email
     public String getEmail() {
         return email;
     }
 
+    // method to get persons password
     public String getPassword() {
         return password;
     }
 
+    // method to get persons name
     public String getName() {
         return name;
     }
 
+    // method to get persons age
     public int getAge() {
         return age;
     }
@@ -67,10 +68,6 @@ public class Person { // class diagram says User.java but i didn't want to break
     public void displayDetails() {
         System.out.println("--- Person Details ---\r\n" + "Name: " + name + "\r\n" + "Age: " + age);
     }
-
-    // public int getSecurity() {
-    // int secLevel;
-    // }
 }
 
 /*
